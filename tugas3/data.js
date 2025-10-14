4f413477eaf046c8a83820eb323c83c5094281e1
Muh.Nabil <nabiloca0602@gmail.com>
2025-10-14 17:57:57 +0700

    Add files via upload

---
+++ tugas3/data.js
@@ -0,0 +1,20 @@
+const film = {
+  judul: "Interstellar",
+  tahunRilis: 2014,
+  sutradara: "Christopher Nolan",
+  genre: ["Sci-Fi", "Adventure", "Drama"],
+
+  tampilkanDetail: function() {
+    return `Judul: ${this.judul}
+Tahun Rilis: ${this.tahunRilis}
+Sutradara: ${this.sutradara}
+Genre: ${this.genre.join(", ")}`;
+  }
+};
+
+console.log("========== DETAIL FILM ==========");
+console.log("Sutradara:", film.sutradara);
+console.log("Genre kedua:", film.genre[1]);
+console.log("Detail Film:\n" + film.tampilkanDetail());
+console.log("=================================");
+
